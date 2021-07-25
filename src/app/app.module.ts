8dcc1978d753d389cb8e38edc0a3248a52ac280e
AnkitaSood <ankita.s33@gmail.com>
2021-07-25 00:28:15 -0500

    5 files modified, 2 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { ListComponent } from './list/list.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [BrowserModule, MatCardModule, MatButtonModule],
+  imports: [BrowserModule, MatCardModule, MatButtonModule, MatButtonToggleModule, MatIconModule],
   declarations: [AppComponent, ListComponent],
   bootstrap: [AppComponent]
 })
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,13 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
-
-  ngOnInit() {
-  }
-
+export class ListComponent {
 }
